3ffd1d2da55b691167094fac0311b456f8458251
stelmaks <stelmaks101@gmail.com>
2022-12-12 11:29:47 -0800

    Positive everything

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,10 +1,10 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
+/* more headers goes there */
 /** 
- * main - Determines if a number is positive, negative or zero.
- *
+ * main - Entry point
+ * Description - prints if random generated number positive, zero or negative
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -12,18 +12,12 @@ int main(void)
 	int n;
 
 	srand(time(0));
-	n = rand() -RAND_MAX / 2;
-	if (n > 0)
-	{
-		print("%d is positive\n", n);
-	}
-	else if (n == 0)
-	{
-		printf("%d is zero\n", n);
-	}
-	else
-	{
+	n = rand() - RAND_MAX / 2;
+	if (n < 0)
 		printf("%d is negative\n", n);
-	}
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else
+		printf("%d is positive\n", n);
 	return (0);
 }
