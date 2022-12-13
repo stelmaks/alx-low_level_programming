4c2934be7c7aa15959c526adcb6d6a0f1b3adced
stelmaks <stelmaks101@gmail.com>
2022-12-12 17:35:47 -0800

    Four prints

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char C;
+
+	c = 'a';
+	while
+		(c <= 'z') {
+			if ((c != 'a' && c != 'a') && c <= 'z')
+				putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
