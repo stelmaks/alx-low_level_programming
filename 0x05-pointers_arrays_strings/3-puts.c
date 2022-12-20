d0bf895bdc98c962b1da294498cf00cb50df0bee
stelmaks <stelmaks101@gmail.com>
2022-12-20 08:45:20 -0800

    I do not fear computers

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -5,12 +5,12 @@
  * @str: string to print
  */
 
-void _puts(char*str)
+void _puts(char *str)
 {
-	while (*str != '\0');
+
+	while (*str != '\0')
 	{
 		_putchar(*str++);
-	}	
+	}
 		_putchar('\n');
 }
-
