ec1a3a84b25765e6c6b177292b6e96b5da89bc62
stelmaks <stelmaks101@gmail.com>
2022-12-20 08:23:54 -0800

    Task two

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -8,14 +8,13 @@
 
 int _strlen(char *s)
 {
-	 int longi = 0;
+	int longi = 0;
 
-	  while (*s != '\0')
-	  {
-		  longi++;
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+	return (longi);
 
-		  s++;
-	  }
-
-	  return (longi);
 }
