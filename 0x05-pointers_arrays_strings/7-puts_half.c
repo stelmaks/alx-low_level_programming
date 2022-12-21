7248dbc35e284f3dfd677fc5e53e65c5ef55ba19
stelmaks <stelmaks101@gmail.com>
2022-12-20 17:56:21 -0800

    Task seven

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -2,26 +2,27 @@
 
 /**
  * puts_half - a function that prints half of a string
- * if odd len, n = (length_of_the_string - 1) / 2
- * @str: input
- * Return: half of input
+ *  if odd len, n = (length_of_the_string - 1) / 2
+ *  @str: input
+ *  Return: half of input
  */
 
 void puts_half(char *str)
 {
-	 int a, n, longi;
+	int a, n, longi; 
 
-	 longi = 0;
+	longi = 0;
 
-	 for (a = 0; str[a] != '\0'; a++)
+	for (a = 0; str[a] != '\0'; a++)
 
-		 longi++;
+		longi++;
 
-	 n = (longi / 2);
-	 if ((longi % 2) == 1)
-		 n = ((longi + 1) / 2);
+	n = (longi / 2);
 
-	 for (a = n; str[a] != '\0'; a++)
-		 _putchar(str[a]);
-	 _putchar('\n');
+	if ((longi % 2) == 1)
+		n = ((longi + 1) / 2);
+
+	for (a = n; str[a] != '\0'; a++)
+		_putchar(str[a]);
+	_putchar('\n');
 }
