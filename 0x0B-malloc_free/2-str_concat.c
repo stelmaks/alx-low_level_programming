d85129c2a581ff740131c6bae234aba7cc20a682
stelmaks <stelmaks101@gmail.com>
2023-01-09 11:56:58 -0800

    Task two

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,44 +1,31 @@
-nclude "main.h"
+#include "main.h"
 #include <stdlib.h>
 
 
 /**
- * str_concat - get ends of input and add together for size
- * @s1: input one to concat
- * @s2: input two to concat
- * Return: concat of s1 and s2
+ * _calloc - Allocates memory for an array of a certain number
+ * of elements each of an inputted byte size.
+ * @nmemb: The number of elements.
+ * @size: The byte size of each array element
+ *
+ * Return: If nmemb = 0, size = 0, or the function fails - NULL.
+ * Otherwise - a pointer to the allocated memory.
  */
 
-char *str_concat(char *s1, char *s2)
+void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *conct;
-	int i, ci;
+	void *mem;
+	char *filler;
+	unsigned int index;
 
-	if (s1 == NULL)
-		s1 = ""
-
-		i = ci = 0
-
-		while (s1[i] != '\0')
-			i++;
-	while (s2[ci] != '\0')
-		ci++;
-	conct = malloc(sizeof(char) * (i + ci + 1));
-
-	if (conct == NULL)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
-	i = ci = 0;
-	while (s1[i] != '\0')
-	{
 
-		conct[i] = s1[i];
-		i++;
-	}
-	while (s2[ci] != '\0')
-	{
-		conct[i] = s2[ci];
-		i++, ci++;
-	}
-	conct[i] = '\0';
-	return (conct);
+	if (mem == NULL)
+		return (NULL);
+	filler = mem;
+
+	for (index = 0; index < (size * nmemb); index++)
+		filler[index] = '\0';
+	return (mem);
 }
