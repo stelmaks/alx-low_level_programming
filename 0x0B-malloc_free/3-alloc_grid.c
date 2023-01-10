575ec492d1a86eee367a0cc4305b192e5eebb390
stelmaks <stelmaks101@gmail.com>
2023-01-10 15:59:05 -0800

    Task three

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -24,7 +24,7 @@ int **alloc_grid(int width, int height)
 
 	for (x = 0; x < height; x++)
 	{
-		mee[x] = malloc(sizeof(int) * width)
+		mee[x] = malloc(sizeof(int) * width);
 			if (mee[x] == NULL)
 			{
 
